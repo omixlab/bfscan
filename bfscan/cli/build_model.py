008a4d9e732e03851ea5103306bec07a975057a9
thgbaum <thg.baum@gmail.com>
2022-12-07 10:49:25 -0300

    xgboost as default estimator

--- bfscan/cli/build_model.py
+++ bfscan/cli/build_model.py
@@ -3,13 +3,14 @@ from bfscan.filter import BFScanFilter
 from bfscan.model import BFScanModel
 from bfscan.utils import read_sequence_file
 from bfscan.logo import logo
-from sklearn.ensemble import ExtraTreesClassifier
+from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from imblearn.under_sampling import RandomUnderSampler
 import numpy as np
 import argparse
 import os
+from sklearn.preprocessing import LabelEncoder
 
 def main():
     print(logo)
@@ -22,7 +23,7 @@ def main():
     arguments = parser.parse_args()
 
     bfscan_filter = BFScanFilter.load(arguments.filter)
-    bfscan_model  = BFScanModel(base_estimator=ExtraTreesClassifier(), filter=bfscan_filter)
+    bfscan_model  = BFScanModel(base_estimator=XGBClassifier(), filter=bfscan_filter)
 
     X_raw = []
     y_raw = []
@@ -47,6 +48,11 @@ def main():
 
     X_ros, y_ros = RandomUnderSampler().fit_resample(X_raw, y_raw)
     X_train, X_test, y_train, y_test = train_test_split(X_ros[:,0], y_ros)
+
+    bfscan_model.le = LabelEncoder()
+    bfscan_model.le.fit(y_train)
+    y_train = bfscan_model.le.transform(y_train)
+    y_test = bfscan_model.le.transform(y_test)
     
     bfscan_model.fit(X_train, y_train)   
     report = classification_report(y_test, bfscan_model.predict(X_test))
